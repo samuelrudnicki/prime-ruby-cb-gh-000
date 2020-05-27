0d5510ffe60e2ad78d787920d577f65ac582c82e
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-27 19:52:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,7 @@
 # Add  code here!
 def prime?(number)
-  (2..number).to_a.each {|value|
-    if(number%value == 0 && value != number || number<2)
-      return false
-    end
+  (2..number).to_a.any? {|value|
+    (number%value == 0 && value != number) || number<2
   }
-  true
 end
   
